387115c525fd5cc02028dddb42373b49eb12f803
Prince Singh <marvelousprince012233@gmail.com>
2022-12-09 22:41:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const exec = require('child_process').exec;
+
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send('This is an AWS EC2 instance running...');
+});
+
+app.post("/compile", (req, res) => {
+  try {
+    fs.writeFile('input.cpp', req.body.input, err => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({
+          status: 'FAILED',
+          message: 'Cannot write input to file.'
+        })
+      } else {
+        console.log('written successsfully');
+
+        // run bash script to compile .cpp to .wasm and .js
+        exec('emcc input.cpp -o out.js && node out.js', (error, stdout, stderr) => {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          console.log('exec error: ' + error);
+          if (stderr) {
+            return res.status(200).json({
+              status: 'SUCCESS',
+              compiledSuccessfully: false,
+              output: stderr
+            })
+          }
+          if (error) {
+            return res.status(500).json({
+              status: 'FAILED',
+              message: 'Internal server error',
+            })
+          }
+          return res.status(200).json({
+            status: 'SUCCESS',
+            compiledSuccessfully: true,
+            output: stdout
+          })
+        });
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'FAILED',
+      message: err.message
+    })
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
